4424c4f3da7a32af36553fd7d23f8e9b3b98b24d
mur-wtag <mahbubur.rahman@welltravel.com>
2016-10-16 09:55:42 +0600

    Almost done

--- lib/toy/robot/simulator/commands.rb
+++ lib/toy/robot/simulator/commands.rb
@@ -3,13 +3,14 @@ module Toy
     module Simulator
       module Commands
         def place(x, y, face)
-          face = Direction.new(x, y, face).facing_toward
-          valid_position?(x.to_i, y.to_i, face)
-          @x, @y, @face = x, y, face
+          valid_position?(x, y, face)
+          @x = x
+          @y = y
+          @face = Direction.new(x, y, face)
         end
 
         def move
-          raise InvalidCommand.new unless @face
+          fail InvalidCommand.new unless @face
           new_x, new_y = @face.step_forward(@x, @y)
           valid_position?(new_x, new_y, @face)
           @x, @y = new_x, new_y
@@ -27,7 +28,7 @@ module Toy
 
         def report
           raise InvalidCommand.new unless @face
-          @output.puts "#{@x},#{@y},#{@face}"
+          @output.puts "#{@x},#{@y},#{@face.faced_to}"
         end
       end
     end
--- lib/toy/robot/simulator/direction.rb
+++ lib/toy/robot/simulator/direction.rb
@@ -2,26 +2,36 @@ module Toy
   module Robot
     module Simulator
       class Direction
+        DIRECTIONS = [:north, :east, :south, :west]
+        DIRECTION_MAPPER = {
+          # faced_to: on_left, on_right
+          north: [3, 1],
+          east: [0, 2],
+          south: [1, 3],
+          west: [2, 0]
+        }
         def initialize(x, y, face)
           @x = x
           @y = y
-          @face = face
+          @face = face.downcase.to_sym
+        end
+
+        def faced_to
+          @face
         end
 
         def step_forward(x, y)
           [@x + x, @y + y]
         end
 
-        def facing_toward
-
-        end
-
         def turn_left
-
+          # Anti-clockwise [N, E, S, W]
+          Direction.new(@x, @y, DIRECTIONS[DIRECTION_MAPPER[@face][0]])
         end
 
         def turn_right
-
+          # Clockwise [W, S, E, N]
+          Direction.new(@x, @y, DIRECTIONS[DIRECTION_MAPPER[@face][1]])
         end
       end
     end
